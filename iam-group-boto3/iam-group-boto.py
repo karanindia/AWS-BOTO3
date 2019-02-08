fc063c4e443c313392ae98b2cee5ffcec1fac14e
Prashant Pokhriyal <ppokhriyal4@gmail.com>
2019-02-08 23:05:15 +0530

    aws-boto3-iam-group:added function for checking the aws authentication credentials

--- iam-group-boto3/iam-group-boto.py
+++ iam-group-boto3/iam-group-boto.py
@@ -1,3 +1,44 @@
 #!/usr/bin/python3
 #Author: Prashant Pokhriyal
-#A BOTO3 wrapper script for managing the iam groups
+#A BOTO3 wrapper script for managing the iam groups.
+
+import os
+import boto3
+from botocore.exceptions import ClientError, NoCredentialsError
+
+#Clear the screen
+os.system('clear')
+
+#Terminal Color Variables
+red='\033[31m'
+green='\033[32m'
+blue='\033[34m'
+orange='\033[33m'
+end='\033[0m'
+
+
+#Function to list the IAM Groups
+def list_iam_groups():
+
+    try:
+
+        client = boto3.client('iam')
+        response = client.list_groups()
+        print(response)
+
+    except NoCredentialsError:
+        print("""{}Error{}: Please Configure your AWS Authentication Credentials.
+       Use '--aws-auth aws_access_key_id aws_secrect_access_key region'""".format(red,end))
+
+    except ClientError as error:
+
+        if error.response['Error']['Code'] == 'InvalidClientTokenId':
+            print("""{}Error{}: Invalid AWS Authentication Credentials.
+       Use '--aws-auth aws_access_key_id aws_secrect_access_key region' """.format(red,end))
+
+
+#Function to create the AWS Authentication Credentials
+        
+
+list_iam_groups()
+    
